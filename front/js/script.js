50b7e1ee8ce056e8fd662ef37b71b5fdc2360a17
Davaille-Leroy Matisse <mat.dl17000@gmail.com>
2021-10-20 15:32:05 +0200

    Affichage page index

--- front/js/script.js
+++ front/js/script.js
@@ -1,8 +1,14 @@
 let url = `http://localhost:3000/api/products`;
-const productName = document.querySelector('.productName');
-const productDescription = document.querySelector('.productDescription');
+let section = document.querySelector('.items');
+// const productName = document.querySelector('.productName');
+// const productDescription = document.querySelector('.productDescription');
+// const img = document.querySelector('article img')
+// const card = document.querySelector('article');
+// const link = document.querySelector('section a')
 
-// Récupération des données API + Répartition sur page Index
+
+
+// Récupération des données API 
 
 const apiCall = async() => {
     await fetch(url)
@@ -10,9 +16,33 @@ const apiCall = async() => {
     .then((data) => (apiData = data))
     console.log(apiData);
 
-    for (i = 0; i < apiData.length; i++) {
-        productName.innerText = apiData[i].name;
-    }
+
+    //  Répartition des données sur page Index
+
+    apiData.forEach(element => {
+
+        newLink = document.createElement('a');
+        newArticle = document.createElement('article');
+
+        newImg = document.createElement('img');
+        newImg.src = element.imageUrl;
+        newImg.alt = element.altTxt;
+
+        newName = document.createElement('h3');
+        newName.classList.add('productName')
+        newName.innerText = element.name;
+
+        newDescription = document.createElement('p');
+        newDescription.classList.add('productDescription')
+        newDescription.innerText = element.description;
+
+
+        section.appendChild(newLink);
+        newLink.appendChild(newArticle);
+        newArticle.appendChild(newImg);
+        newArticle.appendChild(newName);
+        newArticle.appendChild(newDescription);
+    });
 };
 
 apiCall();
