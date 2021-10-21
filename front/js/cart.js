18fab717053506766d8d50cf7d3414e4938e48a4
Davaille-Leroy Matisse <mat.dl17000@gmail.com>
2021-10-21 16:10:02 +0200

    Avancement page produit + panier

---
+++ front/js/cart.js
@@ -0,0 +1,105 @@
+let items = document.querySelector('#cart__items');
+
+function displayBasket() {
+    let product = JSON.parse(localStorage.getItem('product'));
+    console.log(product);
+
+    // Répartition des données
+    for (let i = 0; i < product.length; i++) {
+        items.innerHTML = product.map(`
+        <article class="cart__item" data-id="${product[i].chosenId}">
+            <div class="cart__item__img">
+                <img src="${product[i].chosenImg}" alt="Photographie d'un canapé">
+            </div>
+            <div class="cart__item__content">
+                <div class="cart__item__content__titlePrice">
+                    <h2>${product[i].chosenName}</h2>
+                    <p>${product[i].chosenPrice * product[i].chosenQty + '€'}</p>
+                </div>
+            <div class="cart__item__content__settings">
+                <div class="cart__item__content__settings__quantity">
+                    <p>Qté : </p>
+                    <input type="number" class="itemQuantity" name="itemQuantity" min="1" max="100" value="${product[i].chosenQty}">
+                </div>
+                <div class="cart__item__content__settings__delete">
+                    <p class="deleteItem">Supprimer</p>
+                </div>
+            </div>
+        </div>
+        </article> `)
+    }
+}
+displayBasket();
+
+
+
+
+
+{/* <article class="cart__item" data-id="${product[i].chosenId}">
+<div class="cart__item__img">
+  <img src="${product[i].chosenImg}" alt="Photographie d'un canapé">
+</div>
+<div class="cart__item__content">
+  <div class="cart__item__content__titlePrice">
+    <h2>${product[i].chosenName}</h2>
+    <p>${product[i].chosenPrice * product[i].chosenQty + '€'}</p>
+  </div>
+  <div class="cart__item__content__settings">
+    <div class="cart__item__content__settings__quantity">
+      <p>Qté : </p>
+      <input type="number" class="itemQuantity" name="itemQuantity" min="1" max="100" value="${product[i].chosenQty}">
+    </div>
+    <div class="cart__item__content__settings__delete">
+      <p class="deleteItem">Supprimer</p>
+    </div>
+  </div>
+</div>
+</article> */}
+
+
+
+// newArticle = document.createElement('article');
+//         newArticle.classList.add('cart__item');
+//         newArticle.setAttribute('data-id', product[i].chosenId)
+
+//         items.appendChild(newArticle);
+
+//         // Image
+
+//         newDivImg = document.createElement('div');
+//         newDivImg.classList.add('cart__item__img');
+
+//         newArticle.appendChild(newDivImg);
+
+//         newImg = document.createElement('img');
+//         newImg.src = product[i].chosenImg;
+//         newImg.alt = product[i].chosenAlt;
+
+//         newDivImg.appendChild(newImg);
+
+//         // Prix + Nom
+
+//         newDivContent = document.createElement('div');
+//         newDivContent.classList.add('cart__item__content');
+
+//         newPriceTitle = document.createElement('div');
+//         newPriceTitle.classList.add('cart__item__content__titlePrice')
+        
+//         newName = document.createElement('h2');
+//         newName.innerText = product[i].chosenName;
+//         newPrice = document.createElement('p');
+//         newPrice.innerText = product[i].chosenPrice * product[i].chosenQty + ' € ';
+
+//         newPriceTitle.appendChild(newName);
+//         newPriceTitle.appendChild(newPrice);
+
+
+//         newArticle.appendChild(newDivContent);
+//         newDivContent.appendChild(newPriceTitle)
+
+//         // Quantité
+
+//         newDivSetting = document.createElement('div');
+//         newDivSetting.classList.add('cart__item__content__settings');
+
+//         newQty = document.createElement('div');
--- front/js/product.js
+++ front/js/product.js
@@ -3,6 +3,9 @@ let productName = document.querySelector('#title');
 let productPrice = document.querySelector('#price');
 let productDescription = document.querySelector('#description');
 let productColors = document.querySelector('#colors');
+let quantity = document.querySelector('#quantity');
+let btn = document.querySelector('#addToCart');
+let colorValue = document.querySelector('#colors');
 
 // Récupération de l'ID du produit
 let params = new URL(document.location).searchParams;
@@ -15,7 +18,7 @@ const apiProduct = async () => {
 	await fetch(newUrl)
 		.then((response) => response.json())
 		.then((data) => (productData = data));
-	console.log(productData);
+	// console.log(productData);
 
 	// Répartition des données du produit
 	newImg = document.createElement('img');
@@ -28,12 +31,61 @@ const apiProduct = async () => {
 
 	productDescription.innerText = productData.description;
 
-    // Options couleurs + value
+	// Options couleurs + value
 	for (let i = 0; i < productData.colors.length; i++) {
 		newOption = document.createElement('option');
 		productColors.appendChild(newOption);
 		newOption.innerText = productData.colors[i];
 		newOption.setAttribute('value', productData.colors[i]);
 	}
+
+	// Ajout dans le Local Storage
+
+	function toLocalStorage() {
+		btn.addEventListener('click', (e) => {
+			e.preventDefault();
+
+			let productChosen = {
+				chosenName: productData.name,
+				chosenDescription: productData.description,
+				chosenImg: productData.imageUrl,
+				chosenAlt: productData.altTxt,
+				chosenPrice: productData.price,
+				chosenId: id,
+				chosenQty: quantity.value,
+				chosenColor: colorValue.value
+			};
+
+			// Transformer JSON en objet JS
+			let inLocalStorage = JSON.parse(localStorage.getItem('product'));
+			console.log(inLocalStorage);
+			// Si produits dans LS
+			if (inLocalStorage) {
+				inLocalStorage.push(productChosen);
+				localStorage.setItem('product', JSON.stringify(inLocalStorage));
+			// } else if (h) {
+			// 	// Si pas de produits
+			} else {
+				inLocalStorage = [];
+				inLocalStorage.push(productChosen);
+				localStorage.setItem('product', JSON.stringify(inLocalStorage));
+			}
+		});
+	}
+	toLocalStorage();
 };
+
 apiProduct();
+
+// if (!inLocalStorage) {
+// 	inLocalStorage = [];
+// 	inLocalStorage.push(productChosen);
+// 	localStorage.setItem('product', JSON.stringify(inLocalStorage));
+// // Si produits
+// } else if (!inLocalStorage.some(p => p.chosenId === productChosen.chosenId & p.chosenColor === productChosen.chosenColor)){
+// 	inLocalStorage.push(productChosen);
+// 	localStorage.setItem('product', JSON.stringify(inLocalStorage));
+// } else {
+// 	const newStorage = inLocalStorage.filter(p => p.chosenId === productChosen.chosenId & p.chosenColor !== productChosen.chosenColor);
+// 	newStorage.push(productChosen);
+// 	localStorage.setItem('product', JSON.stringify(newStorage));
